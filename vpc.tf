7d69d7c4f6b47d9e72ca883a1a5ccd51431a125d
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-07-15 08:03:11 +0000

    private subnet

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-# Creating a VPC
+# 1. Creating a VPC
 resource "aws_vpc" "vpc" {
   cidr_block       = "${var.vpc-cidr}"
   instance_tenancy = "default"
@@ -9,7 +9,7 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# Creating an Internet Gateway
+# 2. Creating an Internet Gateway
 resource "aws_internet_gateway" "internet-gateway" {
     vpc_id = aws_vpc.vpc.id
 
@@ -18,7 +18,7 @@ resource "aws_internet_gateway" "internet-gateway" {
     }  
 }
 
-# Create Public Subnet 1
+# 3.1 Create Public Subnet 1
 resource "aws_subnet" "public-subnet-1" {
     vpc_id            = aws_vpc.vpc.id
     cidr_block        = "${public-subnet-1-cidr}"
@@ -30,11 +30,11 @@ resource "aws_subnet" "public-subnet-1" {
     }
 }
 
-# Create Public Subnet 2
+# 3.2 Create Public Subnet 2
 resource "aws_subnet" "public-subnet-2" {
-    vpc_id = aws_vpc.vpc.id
-    cidr_block = "${public-subnet-2-cidr}"
-    availability_zone = "us-east-1b"
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "${public-subnet-2-cidr}"
+    availability_zone       = "us-east-1b"
     map_public_ip_on_launch = true
 
     tags = {
@@ -43,7 +43,7 @@ resource "aws_subnet" "public-subnet-2" {
   
 }
 
-# Create a Route Table and add Public Route
+# 4. Create a Route Table and add Public Route
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.vpc.id
 
@@ -56,13 +56,53 @@ resource "aws_route_table" "public-route-table" {
   }
 }
 
-# Associate Public Subnet 1 to Public Route Table
+# 4.1 Associate Public Subnet 1 to Public Route Table
 resource "aws_route_table_association" "public-subnet-1-route-table-association" {
     subnet_id      = aws_subnet.public-subnet-1.id
     route_table_id = aws_route_table.public-route-table.id
 }
-# Associate Public Subnet 2 to Public Route Table
-resource "" "name" {
-  
+# 4.2 Associate Public Subnet 2 to Public Route Table
+resource "aws_route_table_association" "public-subnet-2-route_table_association" {
+    subnet_id      = aws_subnet.public-subnet-2.id
+    route_table_id = aws_route_table.public-route-table.id  
 }
 
+# 5.1 Create Private Subnet 1
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "${private-subnet-1-cidr}"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false  
+
+  tags = {
+    Name = "Private Subnet 1 | App tier"
+  }
+}
+
+# 5.2 Create Private Subnet 2
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "${private-subnet-2-cidr}"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet 2 | App Tier"
+  }  
+}
+
+# 5.3 Create Private Subnet 3
+resource "aws_subnet" "private-subnet-3" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "${private-subnet-3-cidr}"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false   
+}
+
+# 5.4 Create Private Subnet 4
+resource "aws_subnet" "private-subnet-4" {
+  vpc_id                  =  aws_vpc.vpc.id
+  cidr_block              = "${private-subnet-4-cidr}"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false   
+}
