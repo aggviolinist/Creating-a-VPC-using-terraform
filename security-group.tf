192a9b516addb53d6086d4f28c03054c8ce9fa6c
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-07-17 04:22:17 +0000

    security group

--- security-group.tf
+++ security-group.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "alb-security-group" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0./0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         description = "HTTPS Access"
@@ -22,7 +22,7 @@ resource "aws_security_group" "alb-security-group" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
         Name = "ALB Security Group"
